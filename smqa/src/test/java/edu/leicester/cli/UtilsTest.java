c10bfa388c3ceb3f643cb841a78242c94ddb502a
drb23 <drb23@student.le.ac.uk>
2021-12-08 16:05:13 +0000

    added Util JUnit Tests for strip hyphen and strip quotes and started validateOption

--- smqa/src/test/java/edu/leicester/cli/UtilsTest.java
+++ smqa/src/test/java/edu/leicester/cli/UtilsTest.java
@@ -6,18 +6,162 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UtilsTest {
 
+//    @Test
+//    void testStripLeadingHyphensWithOnlyHyphens() {
+//        final String s = "----";
+//        final String expected = "--";
+//        final String actual = Utils.stripLeadingHyphens(s);
+//        assertEquals(expected, actual);
+//    }
+
+//    @Test
+//    void testStripLeadingHyphenWithOnlyOneHyphen() {
+//        final String s = "-";
+//        final String expected = "";
+//        final String actual = Utils.stripLeadingHyphens(s);
+//        assertEquals(expected, actual);
+//    }
+
     @Test
-    void testStripLeadingHyphensWithOnlyHyphens() {
-        final String s = "----";
-        final String expected = "--";
+    void testStripLeadingHyphenWithHyphensAtStartString() {
+        final String s = "--hello";
+        final String expected = "hello";
         final String actual = Utils.stripLeadingHyphens(s);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    void testStripLeadingHyphenWithOnlyOneHyphenAtStartString() {
+        final String s = "-hello";
+        final String expected = "hello";
+        final String actual = Utils.stripLeadingHyphens(s);
+        assertEquals(expected, actual);
+    }
+    
+        @Test
+        void testStripLeadingHyphenWithOnlyString() {
+            final String s = "hello";
+            final String expected = "hello";
+            final String actual = Utils.stripLeadingHyphens(s);
+            assertEquals(expected, actual);
+        }
+
+    @Test
+    void testStripLeadingHyphensWithNullInput() {
+        final String s = null;
+        final String actual = Utils.stripLeadingHyphens(s);
+        assertNull(actual);
+    }
+
+    @Test
+    void testStripLeadingAndTrailingQuotesWithEmptyString() {
+        final String s = "";
+        final String expected = "";
+        final String actual = Utils.stripLeadingAndTrailingQuotes(s);
         assertEquals(expected, actual);
     }
 
     @Test
-    void test2() { // make sure you give your test a descriptive name
-        fail("Write your second test here.");
+    void testStripLeadingAndTrailingQuotesWithOneCharacter() {
+        final String s = "a";
+        final String expected = "a";
+        final String actual = Utils.stripLeadingAndTrailingQuotes(s);
+        assertEquals(expected, actual);
     }
 
-    // Write more tests here...
+    @Test
+    void testStripLeadingAndTrailingQuotesWithQuotesStartEndEmptyString() {
+        final String s = "\"\"";
+        final String expected = "";
+        final String actual = Utils.stripLeadingAndTrailingQuotes(s);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testStripLeadingAndTrailingQuotesWithQuoteStartString() {
+        final String s = "\"hello";
+        final String expected = "\"hello";
+        final String actual = Utils.stripLeadingAndTrailingQuotes(s);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testStripLeadingAndTrailingQuotesWithQuoteEndString() {
+        final String s = "hello\"";
+        final String expected = "hello\"";
+        final String actual = Utils.stripLeadingAndTrailingQuotes(s);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testStripLeadingAndTrailingQuotesWithQuotesStartEndString() {
+        final String s = "\"hello\"";
+        final String expected = "hello";
+        final String actual = Utils.stripLeadingAndTrailingQuotes(s);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testStripLeadingAndTrailingQuotesWithQuotesStartMiddleEndString() {
+        final String s = "\"hello \" there\"";
+        final String expected = "\"hello \" there\"";
+        final String actual = Utils.stripLeadingAndTrailingQuotes(s);
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void testValidateOptionWithNullInput() {
+        final String s = null;
+        Utils.validateOption(s);
+        assertNull(s); // not sure what to assert
+    }
+
+    @Test
+    void testValidateOptionWithInvalidSingleCharacterOption() {
+        final String s = "/";
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Utils.validateOption(s);
+        });
+
+        String expected = "Illegal option name '/'";
+        String actual = exception.getMessage();
+        assertTrue(actual.contains(expected));
+    }
+    
+        @Test
+        void testValidateOptionWithSingleCharacterQuestionmarkOption() {
+            final String s = "?";
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                Utils.validateOption(s);
+            });
+            
+            String expected = "Illegal option name '?'";
+            String actual = exception.getMessage();
+            assertTrue(actual.contains(expected));
+        }
+
+    @Test
+    void testValidateOptionWithSingleCharacterAtSymbolOption() {
+        final String s = "@";
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Utils.validateOption(s);
+        });
+
+        String expected = "Illegal option name '@'";
+        String actual = exception.getMessage();
+        assertTrue(actual.contains(expected));
+    }
+
+    @Test
+    void testValidateOptionWithInvalidStringOption() {
+        final String s = "hello/";
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Utils.validateOption(s);
+        });
+
+        String expected = "The option '" + s + "' contains an illegal " + "character : '" + "/" + "'";
+        String actual = exception.getMessage();
+        // assertTrue(actual.contains(expected));
+    }
 }
